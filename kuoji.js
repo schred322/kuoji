0dfcf1c9e7033a985a3e9e205a267a6ef95753ae
christian kukta <ckukta@liquidcreations.com>
2020-10-21 18:21:02 -0700

    kuoji housekeeping

--- kuoji.js
+++ kuoji.js
@@ -11,59 +11,45 @@ var cors_options = {
   origin: ['http://localhost:57467', 'http://localhost:1234'],
   optionsSuccessStatus: 200 // For legacy browser support
 };
-
-
-
-app.use(cors(cors_options)); 
-app.use(function(req, res) {
-
-  /*
-  var d = fs.readFile(__dirname + req.url.replace(/\//g, '\\'))  
-            .then(result => {return result}) 
-            .catch(error => console.log('error: ' + error));
-  */
  
-  let file = __dirname + req.url.replace(/\//g, '\\') + '\\index.html';
+app.use(cors(cors_options)); 
+app.use(function(req, res) { 
 
-  if (req.url.includes('.js') || req.url.includes('.html')) {
-    file = __dirname + req.url;
-  } else if (req.url.includes('.')) {
-      file = __dirname + '\\error.html'; 
-  }  
   
-  if (req.url.includes('/www/')) { 
+  var file = __dirname + req.url.replace(/\//g, '\\');
+  var url = req.url;
 
-    //fs.readFile(__dirname + req.url.replace(/\//g, '\\'))
+  if (url.includes('/screens/')) {
+    file = __dirname + req.url.replace(/\//g, '\\') + '\\index.html';
+  } else if (url.includes('/www/')) {
+    file = __dirname + req.url;
+  } else  if (url.includes('.js') || url.includes('.html')) {
+    file = __dirname + req.url.replace(/\//g, '\\');
+  } else if (url.includes('.')) {
+      file = __dirname + '\\www\\error.html'; 
+  } else {
+    file = __dirname + '\\screens\\index.html';
+  }
+
+  if (req.url.includes('/www/')) {  
     fs.readFile(__dirname + '\\www\\master.html') 
       .then(result => {
         fs.readFile(file)
           .then(contents => {  
             res.writeHead(200);   
             res.end(result.toString().replace('{head}', '').replace("{body}", contents.toString()));
-          }); 
-    
-        //res.write(result.toString());
-        //res.write('pull master: ' + contents.toString());
-        //console.log(__dirname + req.url.replace(/\//g, '\\'));
-        /*
-        res.writeHead(200); 
-        res.write(contents.toString().replace('{head}', '').replace('{body}', d.toString()));
-        res.end();
-        */
-
-        //console.log('kuoji www response.');
-        //console.log(screen); 
-        //res.end(contents.toString().replace('{body}', screen)); 
-      });  
- 
+          });  
+      }); 
   }
   else 
   {
+    console.log(file);
+    /* application_view */ 
     fs.readFile(file)
       .then(contents => {  
         res.writeHead(200);   
         res.end(contents.toString());
-      });   
+      });
   } 
 });  
  
@@ -73,5 +59,3 @@ app.listen(config.port, () => {
 
 /* extras */
 // + '<div style="position:absolute; bottom:0; padding:10px; with:100%; background:#f1f1f1;" >Kuoji (2020) with RSI Technology</div>'
-
-
